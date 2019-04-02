500133990f3dd1c8aa9cc70810d5b02b4a86fe87
jinhyy <able0527@gmail.com>
2019-04-02 09:19:39 +0900

    1. 초기세팅

---
+++ 1RestfulService/src/main/java/me/study/sprignio/SprignioApplication.java
@@ -0,0 +1,13 @@
+package me.study.sprignio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprignioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprignioApplication.class, args);
+    }
+
+}
